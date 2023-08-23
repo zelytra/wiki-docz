8f3f71fe2632bb32a1ce2f3a2ff65d72956866e8
Zelytra <72125876+zelytra@users.noreply.github.com>
2023-08-23 14:55:51 +0200

    Add header and navigation bar (#1)

---
+++ frontend/src/layouts/HeaderBar.tsx
@@ -0,0 +1,40 @@
+import React, { type ReactElement } from 'react'
+import { ReactSVG } from 'react-svg'
+import logo from '@/assets/icons/robot.svg'
+import './assets/HeaderBar.scss'
+import { Link } from 'react-router-dom'
+import routes from '@/libs/router'
+import useAxios from '@/libs/http/HTTPAxios'
+import { HTTPRenderWrapper } from '@/libs/http/HTTPRenderWrapper'
+import { type User } from '@/objects/User'
+
+export function HeaderBar (): ReactElement {
+  const userInfo = useAxios({
+    method: 'GET',
+    url: 'user/@me',
+    isProtected: false
+  })
+
+  return (
+        <header>
+            <div className={'side-content'}>
+                <ReactSVG className={'logo'} src={logo}/>
+                <h1>Wiki Docz</h1>
+                <nav>
+                    {routes.map((route, index) => (
+                        <Link key={index} to={route.path}>
+                            <p>{route.name}</p>
+                        </Link>
+                    ))}
+                </nav>
+            </div>
+            <HTTPRenderWrapper state={userInfo}>
+                {(data: User) => (
+                    <>
+                        <img className={'user-icon'} src={data.avatar}/>
+                    </>
+                )}
+            </HTTPRenderWrapper>
+        </header>
+  )
+}
